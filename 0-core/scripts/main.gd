395583ea6cee69e93ccd196acddb6a6dc5108568
Hunter Baker <bakerhb@dukes.jmu.edu>
2025-05-07 19:54:47 -0400

    some ideas defined, minor restructure, and performance tester

---
+++ 0-core/scripts/main.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@onready var tester = PerformanceTester.new()
+
+func _ready():
+	add_child(tester)
+	test_example()
+	
+func test_example():
+	var example = func():
+		var arr = []
+		for i in range(1000):
+			arr.append(randi_range(0, 1000))
+			#arr.append(i)
+		arr.sort()
+	
+	tester.benchmark("test-example", example)
